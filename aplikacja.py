889d748e0affaa128007ce3baeae708dd40204db
prodmtgramx <konrad.loch@gmail.com>
2018-12-01 07:27:03 +0100

    sdfsdf

--- aplikacja.py
+++ aplikacja.py
@@ -1,14 +1,9 @@
 from flask import Flask
-from flask_restful import Resource, Api
-
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route("/")
+def hello():
+     return "Hello World!"
 
-api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+     app.run()
